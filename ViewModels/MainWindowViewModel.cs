6273f5299dcaa5ecfc317550e26f050b92536091
Elina Varenyk <elinavarenyk23@gmail.com>
2026-03-20 21:26:07 +0200

    First commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Tetris_avalonia.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcoe to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Tetris_avalonia.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
